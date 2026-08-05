d705832b7bbc764dfeda8cb31fcf6f677d2a8e6c
Isaac Turpin <Isaac.turpin2@gmail.com>
2026-08-05 15:38:09 +0100

    Created Weapon Class
    Base C++ Weapon class and Blueprint class, with Pistol and Rifle Blueprints derived from base class

---
+++ FPS/Source/FPS/Public/Weapon/Weapon.h
@@ -0,0 +1,34 @@
+﻿// Copyright Isaac Turpin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Weapon.generated.h"
+
+class USkeletalMeshComponent;
+
+UCLASS()
+class FPS_API AWeapon : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AWeapon();
+	
+	USkeletalMeshComponent* GetMesh1P() const;
+	USkeletalMeshComponent* GetMesh3P() const;
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	
+	// Weapon Mesh - First Person View
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh1P;
+	
+	// Weapon Mesh - Third Person View
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USkeletalMeshComponent> Mesh3P;
+};
