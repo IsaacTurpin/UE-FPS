e92fe3772af6212f800f76ee6570d207ae2bf365
Isaac Turpin <Isaac.turpin2@gmail.com>
2026-08-07 09:51:40 +0100

    Base Aim Rotation

--- FPS/Source/FPS/Private/Character/ShooterCharacter.cpp
+++ FPS/Source/FPS/Private/Character/ShooterCharacter.cpp
@@ -64,6 +64,19 @@ void AShooterCharacter::BeginDestroy()
 	}
 }
 
+FRotator AShooterCharacter::GetFixedAimRotation() const
+{
+	FRotator AimRotation = GetBaseAimRotation();
+	if (AimRotation.Pitch > 90.f && !IsLocallyControlled())
+	{
+		// map pitch from [270, 360) to [-90, 0]
+		const FVector2D InRange(270.f, 360.f);
+		const FVector2D OutRange(-90.f, 0.f);
+		AimRotation.Pitch = FMath::GetMappedRangeValueClamped(InRange, OutRange, AimRotation.Pitch);
+	}
+	return AimRotation;
+}
+
 void AShooterCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
--- FPS/Source/FPS/Public/Character/ShooterCharacter.h
+++ FPS/Source/FPS/Public/Character/ShooterCharacter.h
@@ -32,6 +32,9 @@ public:
 	
 	virtual void BeginPlay() override;
 	virtual void BeginDestroy() override;
+	
+	UFUNCTION(BlueprintCallable)
+	FRotator GetFixedAimRotation() const;
 
 protected:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "FPS|Combat")
