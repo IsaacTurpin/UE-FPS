002b9d83c8ac7616c03991b2f22b4820b935849f
Isaac Turpin <Isaac.turpin2@gmail.com>
2026-08-21 11:51:28 +0100

    Processing Eliminations

--- FPS/Source/FPS/Private/Elimination/EliminationComponent.cpp
+++ FPS/Source/FPS/Private/Elimination/EliminationComponent.cpp
@@ -2,6 +2,8 @@
 
 
 #include "Elimination/EliminationComponent.h"
+#include "GameFramework/Pawn.h"
+#include "Player/ShooterPlayerState.h"
 
 
 UEliminationComponent::UEliminationComponent()
@@ -12,7 +14,51 @@ UEliminationComponent::UEliminationComponent()
 
 void UEliminationComponent::OnRoundReported(AActor* Attacker, AActor* Victim, bool bHit, bool bHeadShot, bool bLethal)
 {
-	UE_LOG(LogTemp, Warning, TEXT("Hit: %d, HeadShot: %d, Lethal: %d"), bHit, bHeadShot, bLethal);
+	AShooterPlayerState* AttackerPS = GetPlayerStateFromActor(Attacker);
+	if (!IsValid(AttackerPS)) return;
+	
+	ProcessHitOrMiss(bHit, AttackerPS);
+	
+	if (!bHit) return;
+	
+	AShooterPlayerState* VictimPS = GetPlayerStateFromActor(Victim);
+	if (!IsValid(VictimPS)) return;
+	
+	if (bLethal)
+	{
+		ProcessElimination(bHeadShot, AttackerPS, VictimPS);
+	}
+}
+
+void UEliminationComponent::ProcessElimination(bool bHeadShot, AShooterPlayerState* AttackerPS,
+	AShooterPlayerState* VictimPS)
+{
+	AttackerPS->AddScoredElim();
+	VictimPS->AddDefeat();
+	
+	// headshots and more
+}
+
+void UEliminationComponent::ProcessHitOrMiss(bool bHit, AShooterPlayerState* AttackerPS)
+{
+	if (bHit)
+	{
+		AttackerPS->AddHit();
+	}
+	else
+	{
+		AttackerPS->AddMiss();
+	}
+}
+
+AShooterPlayerState* UEliminationComponent::GetPlayerStateFromActor(AActor* Actor)
+{
+	APawn* Pawn = Cast<APawn>(Actor);
+	if (IsValid(Pawn))
+	{
+		return Pawn->GetPlayerState<AShooterPlayerState>();
+	}
+	return nullptr;
 }
 
 
