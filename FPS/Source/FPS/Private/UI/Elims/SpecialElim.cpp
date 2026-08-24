af8c9419865ad348aee09f668faea26ce07531c3
Isaac Turpin <Isaac.turpin2@gmail.com>
2026-08-24 11:13:22 +0100

    Special Elim Widget and Blueprint Widget

---
+++ FPS/Source/FPS/Private/UI/Elims/SpecialElim.cpp
@@ -0,0 +1,33 @@
+﻿// Copyright Isaac Turpin
+
+
+#include "UI/Elims/SpecialElim.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void USpecialElim::InitialiseWidget(const FString& ElimMessage, UTexture2D* ElimTexture)
+{
+	if (IsValid(Text_ElimMessage))
+	{
+		Text_ElimMessage->SetText(FText::FromString(ElimMessage));
+	}
+	if (IsValid(Image_ElimType))
+	{
+		Image_ElimType->SetBrushFromTexture(ElimTexture);
+	}
+}
+
+void USpecialElim::CentreWidget(UUserWidget* Widget, float VerticalRatio)
+{
+	if (!IsValid(Widget)) return;
+	
+	FVector2D ViewportSize = UWidgetLayoutLibrary::GetViewportSize(Widget);
+	const float VerticalFraction = VerticalRatio == 0.f ? 1.f : VerticalRatio * 2.f;
+	FVector2D CentrePosition(ViewportSize.X / 2.f, VerticalFraction * ViewportSize.Y / 2.f);
+	Widget->SetAlignmentInViewport(FVector2D(0.5f, 0.5f));
+	Widget->SetPositionInViewport(CentrePosition, true);
+	
+	
+}
---
+++ FPS/Source/FPS/Public/UI/Elims/SpecialElim.h
@@ -0,0 +1,31 @@
+﻿// Copyright Isaac Turpin
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SpecialElim.generated.h"
+
+
+class UImage;
+class UTextBlock;
+
+UCLASS()
+class FPS_API USpecialElim : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> Text_ElimMessage;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> Image_ElimType;
+	
+	void InitialiseWidget(const FString& ElimMessage, UTexture2D* ElimTexture);
+	
+	UFUNCTION(BlueprintCallable)
+	static void CentreWidget(UUserWidget* Widget, float VerticalRatio = 0.f);
+	
+};
