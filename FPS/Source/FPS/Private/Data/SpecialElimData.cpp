f71ec7a79eb1dcf85bbeecd61b1a5c884d32d293
Isaac Turpin <Isaac.turpin2@gmail.com>
2026-08-24 11:33:54 +0100

    Special Elim Data

---
+++ FPS/Source/FPS/Private/Data/SpecialElimData.cpp
@@ -0,0 +1,4 @@
+﻿// Copyright Isaac Turpin
+
+
+#include "Data/SpecialElimData.h"
--- FPS/Source/FPS/Public/Player/ShooterPlayerState.h
+++ FPS/Source/FPS/Public/Player/ShooterPlayerState.h
@@ -7,6 +7,7 @@
 #include "ShooterPlayerState.generated.h"
 
 
+class USpecialElimData;
 enum class ESpecialElimType : uint16;
 
 UCLASS()
@@ -45,6 +46,9 @@ public:
 	
 	UFUNCTION(Client, Reliable)
 	void Client_SpecialElim(const ESpecialElimType& SpecialElim, int32 SequentialElimCount, int32 StreakCount, int32 ElimScore);
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "FPS|SpecialElims")
+	TObjectPtr<USpecialElimData> SpecialElimData;
 	
 private:
 	
